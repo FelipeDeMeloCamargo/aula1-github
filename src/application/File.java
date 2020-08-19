197623d2cd6dcef83ff73cb7184a6f92aa493fd7
Felipe Melo <felipe.camargo@luizalabs.com>
2020-08-19 20:00:14 -0300

    FileWriter

--- src/application/File.java
+++ src/application/File.java
@@ -1,27 +1,26 @@
 package application;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
 import java.io.IOException;
 
 public class File {
 
 	public static void main(String[] args) {
 		
-		String path = "C:\\Users\\mlwkt500\\Documents\\in.txt"; //local do arquivo
+		String [] lines = new String[] {"Good Morning","Good afternoon","Google night"};
 		
-		try (BufferedReader br = new BufferedReader(new FileReader(path))){ //declaracao da variavel br
-			String line = br.readLine();//serve para ler 1 linha do arquivo
-			 
-			while (line != null) { //while para verificar se a linha que eu solicitei para ler, não é a ultima
-				System.out.println(line);
-				line = br.readLine(); // e então lerá novamente o que estiver no arquivo
+		String path = "C:\\Users\\mlwkt500\\Documents\\out.txt";
+		
+		try (BufferedWriter bw = new BufferedWriter(new FileWriter(path))){ //colocando o true apos a variavel, ele complementa o arquivo ao inves de recria-lo
+			for (String line : lines) {
+				bw.write(line); //Insere uma linha
+				bw.newLine(); //dá uma quebra de linha
 			}
 		}
-		catch (IOException e ) {
-			System.out.println("Error: " + e.getMessage());
+		catch (IOException e) {
+			e.printStackTrace();// irá imprimir o erro
 		}
-		
 	}
 
 }
